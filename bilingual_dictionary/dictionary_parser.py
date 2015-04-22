a3fedee8dfa3f5bc5890e0efe470836385f34f73
Luke Carlson <jlukecarlson@gmail.com>
2015-04-22 19:41:49 -0400

    going through all data. need to work on waiting or hiding agent

--- bilingual_dictionary/dictionary_parser.py
+++ bilingual_dictionary/dictionary_parser.py
@@ -3,36 +3,40 @@ from bs4 import BeautifulSoup
 import sys
 import string
 import codecs
+import traceback
 
 alphabet = string.ascii_lowercase #'abcdefghijklmnopqrstuvwxyz'
+# we already have a,b,c so start at d
+alphabet = alphabet[3:]
 browse_page = "http://www.shabdkosh.com/te/browse/"
 base_url = "http://www.shabdkosh.com"
-try :
-    web_page = urllib2.urlopen(browse_page).read()
-    soup = BeautifulSoup(web_page)
-    engl_words = soup.find_all(target='_blank')
-    f = codecs.open("a_telugu.csv", "w", "utf-8")
-    i = 0
-    for e in engl_words:
-        i += 1
-        if i == 10:
-            break
-        e_text =  e.get_text()
-        e_url = e['href']
-        try:
-            url = base_url + e_url.replace(" ", "%20")
-            e_web_page = urllib2.urlopen(url).read()
-            e_soup = BeautifulSoup(e_web_page)
-            first_trans = e_soup.find("a", class_="in").get_text()
-            f.write("%s,%s\n" % (e_text, first_trans))
-            sys.stderr.write("%s,%s\n" % (e_text, first_trans))
-        except:
-            sys.stderr.write("Error for word %s and url %s !\n" % (e_text, url))
-    
+for letter in alphabet:
+    sys.stderr.write(letter + "\n")
+    try :
+        req = urllib2.Request(browse_page + letter, headers={ 'User-Agent': 'Mozilla/5.0' })
+        web_page = urllib2.urlopen(req).read()
+        soup = BeautifulSoup(web_page)
+        engl_words = soup.find_all(target='_blank')
+        f = codecs.open(letter + "_telugu.csv", "w", "utf-8")
+        for e in engl_words:
+            e_text =  e.get_text()
+            e_url = e['href']
+            try:
+                url = base_url + e_url.replace(" ", "%20")
+                req = urllib2.Request(url, headers={ 'User-Agent': 'Mozilla/5.0' })
+                e_web_page = urllib2.urlopen(req).read()
+                e_soup = BeautifulSoup(e_web_page)
+                first_trans = e_soup.find("a", class_="in").get_text()
+                f.write("%s,%s\n" % (e_text, first_trans))
+                sys.stderr.write("%s,%s\n" % (e_text, first_trans))
+            except:
+                sys.stderr.write("Error for word %s and url %s !\n" % (e_text, url))
+                traceback.print_exc(file=sys.stdout)
+        f.close()
     #web_page = urllib2.urlopen("http://www.shabdkosh.com/te/translate/abundant/abundant-meaning-in-Telugu-English").read()
     #soup = BeautifulSoup(web_page)
     #c = soup.find("a", class_="in").get_text()
-except urllib2.HTTPError :
-    sys.stderr.write("HTTPERROR!\n")
-except urllib2.URLError :
-    sys.stderr.write("URLERROR!\n")
+    except urllib2.HTTPError :
+        sys.stderr.write("HTTPERROR!\n")
+    except urllib2.URLError :
+        sys.stderr.write("URLERROR!\n")
